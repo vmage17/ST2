c6ad6b4307345f1fbf52e5a8bbbe9eab3ea6ee2e
vmage17 <70533975+vmage17@users.noreply.github.com>
2023-05-09 18:28:07 +0200

    Add files via upload

---
+++ SzkolenieTechniczne3.Cinema.Service/Command/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkolenieTechniczne3.Cinema.Service.Command
+{
+    public interface ICommand
+    {
+
+    // tu skonczyłem
+
+    }
+}
---
+++ SzkolenieTechniczne3.Cinema.Service/Result.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkolenieTechniczne3.Cinema.Service
+{
+    public class Result
+    {
+
+        private Result(bool isSuccess, string message, IEnumerable<Error> errors)
+        {
+            IsSuccess = isSuccess;
+            IsFailure = !isSuccess;
+            Message = message;
+            Errors = errors;
+        }
+
+        public string Message { get; }
+        public bool IsFailure { get; }
+        public bool IsSuccess { get; }
+
+        public IEnumerable<Error> Errors { get; }
+        public static Result Fail(String message)
+            => new Result(false, message, Enumerable.Empty<Error>());
+
+
+        public static Result Ok()
+            => new Result(true, "", Enumerable.Empty<Error>());
+
+        public class Error
+        {
+            public Error(string propertyName, string message)
+            {
+                PropertyName = propertyName;
+                Message = message;
+            }
+
+            public string PropertyName { get; }
+            public string Message { get; }
+        }
+    }
+}
